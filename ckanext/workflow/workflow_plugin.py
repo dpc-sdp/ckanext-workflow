ac246eab00dcdaaecccdd2ec9bdca6099138be18
Mark Calvert <markcalvert84@gmail.com>
2021-10-21 12:18:44 -0500

    Removed IConfigurer interface which should only be in DataVicHierarchyForm

--- ckanext/workflow/workflow_plugin.py
+++ ckanext/workflow/workflow_plugin.py
@@ -15,12 +15,6 @@ class WorkflowPlugin(plugins.SingletonPlugin):
     plugins.implements(plugins.IPackageController, inherit=True)
     plugins.implements(plugins.IAuthFunctions)
     plugins.implements(plugins.IActions)
-    plugins.implements(plugins.IConfigurer)
-
-    # IConfigurer interface #
-    def update_config(self, config):
-        ''' Setup the template directory '''
-        toolkit.add_template_directory(config, 'templates')
 
     # IAuthFunctions
     def get_auth_functions(self):
